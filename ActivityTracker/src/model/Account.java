ebcd1ba5ba3649cb2320403f2eeebb523ff08d24
sammids <sdeakinsharp@mun.ca>
2018-11-27 16:56:00 -0330

    Added in friends methods for the Account class

--- ActivityTracker/src/model/Account.java
+++ ActivityTracker/src/model/Account.java
@@ -9,6 +9,10 @@ public class Account {
 	private String password;
 	private ArrayList<Device> devices;
 	private ArrayList<Activity> activities;
+	private ArrayList<Account> friends;
+	private ArrayList<Object> info;
+	//private int age;
+	//private String gender;
 	
 	public Account() {
 	
@@ -19,6 +23,10 @@ public class Account {
 		this.password = password;
 		this.devices = new ArrayList<Device>();
 		this.activities = new ArrayList<Activity>();
+		//this.age = age;
+		//this.gender = gender;
+		this.friends = new ArrayList<Account>();
+		this.info = new ArrayList<Object>();
 	}
 	
 	public String getUserName() {
@@ -36,7 +44,54 @@ public class Account {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	//start of changes 
+	//commenting out methods involving age and gender until constructor in Model matches
+		/*public int getAge() {
+			return age;
+		}
 
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public String getGender() {
+			return gender;
+		}
+
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		*/
+		public ArrayList<Object> getInfo(){
+			return info;
+		}
+		public void setInfo() {
+			info.add(this.userName);
+			//info.add(this.age);
+			//info.add(this.gender);
+		}
+		
+		public ArrayList<Account> getFriends() {
+			return friends;
+		}
+		
+		public void addFriend(Account a) {
+			if (!friends.contains(a)) {
+				friends.add(a);
+			}
+		}
+		public void removeFriend(Account a) {
+			friends.remove(a);
+		}
+		
+		public ArrayList<Activity> getFriendsActivities(Account a) {
+			return a.getActivities();
+		}
+		public ArrayList<Object> getFriendsInfo(Account a){
+			return a.getInfo();
+		}
+		
+		//end of changes
+		
 	public void addDevice(Device device) {
 		devices.add(device);
 		activities.addAll(device.getActivities());
